ec778679fd664633ed0bb1d6119a2a601a09da5c
Nestor Bonilla <nestorbonilla@Nestors-MacBook-Pro.local>
2020-10-31 08:21:46 -0500

    adding features to stripe checkout

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -2,10 +2,14 @@ const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 const inventory = require('../data/products.json');
 
 exports.handler = async ({ body }) => {
-    const { sku, quantity } = JSON.parse(body);
+    const { email, amount } = JSON.parse(body);
+    console.log('email ', email);
+    console.log('amount ', amount);
     const product = inventory.find((p) => p.sku === "RXC001");
 
     const session = await stripe.checkout.sessions.create({
+        customer_email: email,
+        submit_type: 'donate',
         billing_address_collection: 'auto',
         shipping_address_collection: {
             allowed_countries: ['US', 'CA'],
@@ -16,9 +20,12 @@ exports.handler = async ({ body }) => {
             price_data: {
               currency: 'usd',
               product_data: {
-                name: 'Donation',
+                name: 'Donation for initiative',
+                description: 'Full description of the initiative.',
+                images: ['https://api.downtownstimulus.com/static/2_cover.jpg'],
+
               },
-              unit_amount: 2000,
+              unit_amount: Number.parseInt(amount) * 100,
             },
             quantity: 1,
           },
